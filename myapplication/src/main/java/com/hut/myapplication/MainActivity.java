016cb4deb960fc801d22d0e6d1a7e09dfc885b1a
Just__ <374901588@qq.com>
2016-05-03 17:53:12 +0800

    初始化配置-Just

--- myapplication/src/main/java/com/hut/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/hut/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.hut.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("测试","++++++++++++++");
     }
 }
